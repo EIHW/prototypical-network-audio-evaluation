f593135183b3c4a43d1d3d13a0a31fa5b3ea0e77
aliceebaird <alice.e.baird@gmail.com>
2021-06-08 10:38:51 +0200

    aug

--- augmentation_options.py
+++ augmentation_options.py
@@ -9,6 +9,13 @@ import librosa.display
 from specAugment import spec_augment_tensorflow
 import sys
 
+DATAPATH = 'source_data/train/*/*.wav'
+OUTPUTFOLDER = 'spectrograms/'
+os.makedirs(OUTPUTFOLDER, exist_ok=True) 
+
+NOISE = True
+TIME True 
+SPECAUG = False
 
 def input_audio(wav_file, normalise):
 	audio, sr = librosa.load(wav_file, sr=16000)
@@ -42,51 +49,37 @@ def time_aug(data, sampling_rate, shift_max, shift_direction):
     return augmented_data
     
 
-def graph_spectrogram(filename,wav_file,output_folder,noise,time,specaug):
+def extract_spectrogram(filename,wav_file,OUTPUTFOLDER,NOISE,TIME,SPECAUG):
 	try:
 		y, sr = input_audio(wav_file,False)
 
 	except:
 		print('file corrupt')
-	if noise:
+	if NOISE:
 		y = noise_aug(y, 0.01)
-	if time: 
+	if TIME: 
 		y = time_aug(y, sr, 0.1, 'right')
+	
 	S = librosa.feature.melspectrogram(y=y,sr=sr,n_mels=256,hop_length=128,fmax=2000)
 	S = librosa.power_to_db(S)
 	
-	if specaug: 
-    		# reshape spectrogram to [batch_size, time, frequency, 1]
+	if SPECAUG: 
 		shape = S.shape
 		mel_spectrogram = np.reshape(S, (-1, shape[0], shape[1], 1))
-
-    		# Show Raw mel-spectrogram
 		S=spec_augment_tensorflow.spec_augment(S)
 	
 	fig = plt.figure(num=None, figsize=(1.50, 1.50),dpi=100)
-
 	img = librosa.display.specshow(S, sr=sr,fmax=2000,cmap='viridis')
-
 	plt.axis('off')
 	plt.margins(0)
-
                                                      
-	plt.savefig(output_folder+filename+'.png',transparent=True)
+	plt.savefig(OUTPUTFOLDER+filename+'.png',transparent=True)
 	plt.close()
 
 
-	
-	
-output_folder = 'spectrograms/'
-os.makedirs(output_folder, exist_ok=True) 
+if __name__ == '__main__':
+	for file in glob.glob(DATAPATH):
+	    filename = file.split('/')[3]
+	    print(f'augment: {filename}')
+	    extract_spectrogram(filename, file, OUTPUTFOLDER,NOISE,TIME,SPECAUG)
 
-# ~ for file in glob.glob('generated_by_wavegan/*/*.wav'):
-for file in glob.glob('source_data/train/*/*.wav'):
-    filename = file.split('/')[3]
-    print(f'augment: {filename}')
-    try:
-# 	filename,wav_file,output_folder,noise,time,specaug
-        graph_spectrogram(filename, file, output_folder,False,False,True)
-    except:
-        print(f'issue with {filename}')
-        continue
